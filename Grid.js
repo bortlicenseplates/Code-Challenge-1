b50453625f05ed8fa38154860f22bcfba633f843
James <jamer@MacBook-Pro-2.local>
2019-04-12 00:34:40 +1000

    fixed silly bug

--- Grid.js
+++ Grid.js
@@ -1,21 +1,24 @@
 const Vector = require('./Vector');
 module.exports = class Grid{
 	constructor(sx,sy){
-		this.origin = new Vector();
+		this.origin = new Vector(0,0);
 		this.size = new Vector(sx,sy);
+		console.log(this.origin);
 	}
 
 	isInsideMap(x,y){
 		const XInside = isInRange(x, this.origin.x, this.size.x);
 		const YInside = isInRange(y, this.origin.y, this.size.y);
-		if(XInside && YInside) return true;
+		console.log()
+		if(XInside && YInside)
+			return true;
 		return false;
 	}
 }
 
 //hidden from imports
 const isInRange = (input, min, max) => {
-	if (input >= min || input <= max)
+	if (input >= min && input <= max)
 		return true;
 	return false;
 }
