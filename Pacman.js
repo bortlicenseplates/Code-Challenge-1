5f6bab1542a9f573ee5133dced7eb3e47ee5550d
James <jamer@MacBook-Pro-2.local>
2019-04-12 09:48:23 +1000

    abstracted helper functions into helpers.js

--- Pacman.js
+++ Pacman.js
@@ -1,11 +1,11 @@
 // const grid = require('./Grid');
 const Vector = require('./Vector');
-
+const helpers = require('./helpers')
 
 module.exports = class Pacman{
 	constructor(x, y, direction, grid){
 		this.position = new Vector(x, y);
-		this.direction = new directionToVector(direction);
+		this.direction = new helpers.directionToVector(direction);
 		this.grid = grid;
 	}
 	turnLeft(){
@@ -26,39 +26,8 @@ module.exports = class Pacman{
 		const info = {
 			x: this.position.x,
 			y: this.position.y,
-			direction: vectorToDirection(this.direction)
+			direction: helpers.vectorToDirection(this.direction)
 		}
 		console.log(`${info.x}, ${info.y}, ${info.direction}`);
 	}
 }
-
-const vectorDirection = {
-	north: [0,1],
-	east: [1,0],
-	south: [0,-1],
-	west: [-1,0]
-};
-
-function directionToVector(direction){
-	try {
-		const vectorProps = vectorDirection[direction];
-		return new Vector(vectorProps[0], vectorProps[1]);
-	} catch(e) {
-		console.log(`${direction} is not a valid direction. Please try placing Pacman again.`);
-	}
-
-}
-function vectorToDirection(vector){
-	const vecArr = new Array(vector.x, vector.y);
-	const keys = Object.keys(vectorDirection);
-	return keys.find(key => arrayIsEqual(vectorDirection[key], vecArr)).toUpperCase();
-}
-function arrayIsEqual (arr1, arr2){
-	if (arr1.length !== arr2.length) return false;
-	for(index = 0; index < arr2.length; index++){
-      if (arr1[index] !== arr2[index]) return false;
-    }
-  	return true;
-}
-
-// Finish first. holds pacman state but not behaviour.
